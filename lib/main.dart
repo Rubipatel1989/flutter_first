2d73307471a5138c566287bda43673ce2f5ff7e2
Pawan Kumar <pawank@exeire.com>
2025-03-17 11:32:02 +0530

    Add Data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+const String apiBaseUrl = "https://jsonplaceholder.typicode.com/todos/";
 
 void main() {
   runApp(const MyApp());
@@ -6,15 +10,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter API Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Users List'),
     );
   }
 }
@@ -28,12 +33,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List todos = [];
+  bool isLoading = true;
+  String selectedTodo = "";
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    fetchTodos();
+  }
+
+  Future<void> fetchTodos() async {
+    try {
+      final response =
+          await http.get(Uri.parse(apiBaseUrl)).timeout(Duration(seconds: 10));
+      if (response.statusCode == 200) {
+        setState(() {
+          todos = jsonDecode(response.body);
+          isLoading = false;
+        });
+      } else {
+        setState(() {
+          isLoading = false;
+        });
+      }
+    } catch (error) {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  Future<void> fetchTodoDetails(int id) async {
+    final response = await http.get(Uri.parse("$apiBaseUrl$id"));
+    if (response.statusCode == 200) {
+      setState(() {
+        selectedTodo = jsonDecode(response.body)['title'];
+      });
+    } else {
+      throw Exception('Failed to load todo details');
+    }
+  }
+
+  Future<void> addTodo() async {
+    final response = await http.post(
+      Uri.parse(apiBaseUrl),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({"title": "New Task", "completed": false}),
+    );
+    if (response.statusCode == 201) {
+      setState(() {
+        todos.insert(0, jsonDecode(response.body));
+      });
+    } else {
+      throw Exception('Failed to add todo');
+    }
   }
 
   @override
@@ -43,25 +97,39 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: todos.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(todos[index]['title']),
+                        subtitle: Text("ID: ${todos[index]['id']}"),
+                        onTap: () => fetchTodoDetails(todos[index]['id']),
+                      );
+                    },
+                  ),
+                ),
+                selectedTodo.isNotEmpty
+                    ? Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text(
+                          "Selected Todo: $selectedTodo",
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                      )
+                    : Container(),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: addTodo,
+        tooltip: 'Add Todo',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
